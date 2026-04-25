1d289911c84b3d8f0adc9a842215c5c1557ded9c
Angadi Charan Umesh <cs1240954@iitd.ac.in>
2026-04-25 16:39:52 +0000

    added api declarations for assignment

--- leveldb/include/leveldb/db.h
+++ leveldb/include/leveldb/db.h
@@ -7,6 +7,8 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <utility>
+#include <vector>
 
 #include "leveldb/export.h"
 #include "leveldb/iterator.h"
@@ -86,6 +88,15 @@ class LEVELDB_EXPORT DB {
   // May return some other Status on an error.
   virtual Status Get(const ReadOptions& options, const Slice& key,
                      std::string* value) = 0;
+
+  virtual Status Scan(const ReadOptions& options, const Slice& start_key,
+                      const Slice& end_key,
+                      std::vector<std::pair<std::string, std::string>>* result) = 0;
+
+  virtual Status DeleteRange(const WriteOptions& options, const Slice& start_key,
+                             const Slice& end_key) = 0;
+
+  virtual Status ForceFullCompaction() = 0;
 
   // Return a heap-allocated iterator over the contents of the database.
   // The result of NewIterator() is initially invalid (caller must
